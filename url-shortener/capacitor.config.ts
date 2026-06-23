0ca60ab066338797e8c73b4cb797c2e31dfa8b3a
Jawad1529 <syedjawadshah00@gmail.com>
2026-06-23 16:37:38 +0500

    Ionic React app started

---
+++ url-shortener/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'url-shortener',
+  webDir: 'dist'
+};
+
+export default config;
